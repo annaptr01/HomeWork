3833ef57d9f51cf50fb7d9446008e4d03dbd0384
anna <annetakaulitz@mail.ru>
2023-04-21 18:32:57 +0300

    added new files and folder for homework

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿//Задача 2.
+//Напишите программу, которая на вход принимает два числа
+//и выдет, какое число большее, а какое меньшее
+
+/*
+Console.WriteLine ("Input your 1st number: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input your 2nd number: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+if(num_1 > num_2)
+{
+    Console.WriteLine("You max number is: " + num_1 + " and you min number is: " + num_2);
+}
+else
+{
+    Console.WriteLine("Your max number is: "+ num_2 + " and your min number is: " + num_1); 
+}
+*/
+
+
+//Задача 4
+//Напишите программу, которая принимает на вход три числа
+//и выдает максимальное из этих чисел.
+
+//запросить 3 числа
+//ввести перемунную max
+//сделать цикл
+/*
+Console.WriteLine ("Input your 1st number: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input your 2nd number: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input your 3rd number: ");
+int num_3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+while(max )
+{
+    Console.WriteLine("Your max number is: "+ );
+}
+
+*/
+
+
+
+//Задача 6.
+//Напишите программу, которая на вход принимает число и выдает, 
+//является ли число четным (делится ли оно на 2 без остатка)
+
+Console.WriteLine ("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2)
+{
+     Console.WriteLine("Your number " + num + "is even-numbered");
+}
+else
+{
+    Console.WriteLine("Your number " + num + "is not even-numbered");
+}
+
+
+
+//Задача 8.
+//Напишите программу, которая на вход принимает число(N), а на выходе
+//показывает все четные числа от 1 до N.
+
+
+
